37b894a42b9eeb161dd43cc5a84d60af1149888a
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-07-28 21:56:21 +0700

    Mencari data di format txt

--- dit2.py
+++ dit2.py
@@ -1,1 +1,19 @@
 import os
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
+
+filname = input('Masukan file >')
+fl = open(filname)
+data = {}
+for i in fl:
+    if i.startswith('From:'):
+        key = i.rstrip().split()[1]
+        data[key] = data.get(key, 0) + 1
+value = []
+for i in data.values():
+    value.append(i)
+for i, z in data.items():
+    if data[i] == max(value):
+        print(i, z)
