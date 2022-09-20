ce5bef60d08381f457feda64d68ee7ff58d740dd
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-09-20 21:05:11 +0700

    Kripto Moderen 1

--- lth17.py
+++ lth17.py
@@ -1,1 +1,22 @@
-#
+# Kripto Moderen
+# alat yang  di gunakan
+"""
+-> ord() -> mengubah ke dalam bentuk desimal
+-> bin() -> mengubah ke dalam bentuk Biner
+-> hex() -> mengubah ke dalam bentuk heksa
+
+"""
+
+
+def fungsi(x, key):
+    string = ""
+    for i in x:
+        a = ord(i) ^ ord(key)
+        string += chr(a)
+    return string
+
+
+nilai = fungsi("Hallo semuanya apa kabar ayo maling BUKU", "K")
+print('ciperteks : {}'.format(nilai))
+nilai2 = fungsi(nilai, "K")
+print('palintex : {}'.format(nilai2))
